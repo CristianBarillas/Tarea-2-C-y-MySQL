31d6e8495ad6b2dc4a2aea242722105da950b5f6
Cristian <cristianbarillas23@gmail.com>
2024-05-23 20:56:11 -0600

    se agrega el CRUD de la tabla estudiante

---
+++ Persona.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <mysql.h>
+using namespace std;
+class Persona {
+	//Atributos
+protected: string nombres, apellidos, direccion, fecha_nacimiento;
+		 int telefono;
+		 //constructor 
+protected: 
+	persona () {
+	}
+	Persona (string nomb, string ape, string dir, int tel,string fn) }
+	nombres = nom;
+	apellidos = ape;
+	direccion = dir;
+	telefono = tel;
+	fecha_nacimiento = fn
+{
+};
+
